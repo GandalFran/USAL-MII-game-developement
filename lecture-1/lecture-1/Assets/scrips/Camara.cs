128906f3b00c9dd39ad262302e6cda76bc7bb44d
gandalfran <franpintosantos@gmail.com>
2020-11-27 15:56:25 +0100

    .

---
+++ lecture-1/lecture-1/Assets/scrips/Camara.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camara : MonoBehaviour
+{
+    public GameObject gameObject;
+    protected Vector3 posicionOrigenObjeto;
+    protected Vector3 posicionOrigenCamara;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.posicionOrigenObjeto = gameObject.transform.position;
+        this.posicionOrigenCamara = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = (this.gameObject.transform.position - this.posicionOrigenObjeto) + this.posicionOrigenCamara;
+    }
+}
---
+++ lecture-1/lecture-1/Assets/scrips/Movimiento.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Movimiento : MonoBehaviour
+{
+
+	//Poner publico para poder ser editado desde unity
+	public float velocidad = 1000;
+	public Text guiText;
+	protected int puntos;
+	protected float tiempo;
+	protected Rigidbody rig;
+
+	// Use this for initialization
+	void Start()
+	{
+		puntos = 0;
+		tiempo = Time.time;
+		guiText.text = "";
+		rig = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Puntos")
+		{
+			puntos++;
+			other.gameObject.SetActive(false);
+		}
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.tag == "Final")
+		{
+			float puntuacion = (Time.time - tiempo) - puntos * 5;
+			guiText.text = "Nivel superado!!: " + puntuacion;
+			//Destroy (this.rigidbody);
+			this.rig.isKinematic = true;
+		}
+	}
+
+	void FixedUpdate()
+	{
+		Vector3 direccion = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		rig.AddForce(direccion * velocidad * Time.deltaTime);
+		//Camera.main.transform.LookAt (transform);
+	}
+}
